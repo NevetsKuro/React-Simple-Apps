7fca587840644c196eea2c82853b9f9ac05f86ab
stark1996 <stevenfernz96@gmail.com>
2020-11-07 13:49:56 +0530

    Updates

--- src/Loading.js
+++
@@ -1,6 +0,0 @@
-import react from 'react';
-
-const Loading = () => {
-    return react.createElement('h1',null,react.createElement('i',null,'Still Under Construction..'));
-}
-export default Loading;
